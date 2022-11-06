763532eaf974e90038d9757c836cd1918b555047
Philip Tofte Nielsen <philiptofte@hotmail.com>
2022-11-06 13:28:11 +0100

    First barebones working example

--- src/main.py
+++ src/main.py
@@ -1,20 +1,64 @@
+import torch
+import numpy as np
+import random
+
+import hydra
+from hydra.utils import get_original_cwd
+from omegaconf import DictConfig
 
 
+from qnet_train import train_model
+from model import QNetwork
 
 class Session:
 
     def __init__(self, config):
-
-        train_params = config.training
+        # Retrieve data from config files
+        session_params = config.session
+        #train_params = config.training
         model_params = config.model
-        eval_params = config.evaluate
+        #eval_params = config.evaluate
+
+        # Setup seeds for predictability
+        torch.manual_seed(session_params["seed"])
+        random.seed(session_params['seed'])
+        np.random.seed(session_params['seed'])
+
+        # Setup GPU
+        if torch.cuda.is_available():
+          self.dev = "cuda:0"
+        else:
+          self.dev = "cpu"
+
+        self.model = QNetwork(n_inputs=model_params['n_inputs'],
+                              n_outputs=model_params['n_outputs'],
+                              learning_rate=model_params['learning_rate'])
+
+        if session_params['command'] == 'train':
+            print('train!')
+            self.train()
+        elif session_params['command'] == 'evaluate':
+            self.evaluate()
+        else:
+            raise Exception('Unknown command')
+
+    def train(self):
+        pass
+
+    def evaluate(self):
+        pass
+
+    def setup_model(self):
+        pass
 
 
 
 
-
-
+@hydra.main(config_path="hparams/", config_name="default_config")
+def objective(config: DictConfig):
+    sess = Session(config)
+    return 0
 
 
 if __name__ == '__main__':
-    pass
+    objective()
--- src/qnet_train.py
+++ src/qnet_train.py
@@ -8,70 +8,68 @@ import gym
 from skimage import io
 from model import QNetwork, get_parameters
 
-env = gym.make('ALE/Asterix-v5',full_action_space=False, obs_type='grayscale')
 
-if torch.cuda.is_available():  
-  dev = "cuda:0" 
-else:  
-  dev = "cpu"  
+
+def train_model(dev):
+    env = gym.make('ALE/Asterix-v5',full_action_space=False, obs_type='grayscale')
 
 
 
 # train Q-network
-num_episodes = 100
-episode_limit = 300
-gamma = 0.9 # discount rate
-val_freq = 1 # validation frequency
-epsilon_start = 1.0
+    num_episodes = 100
+    episode_limit = 300
+    gamma = 0.9 # discount rate
+    val_freq = 1 # validation frequency
+    epsilon_start = 1.0
 
-n_inputs, n_outputs, learning_rate = get_parameters() 
-qnet = QNetwork(n_inputs, n_outputs, learning_rate)
-qnet.to(torch.device(dev))
+    n_inputs, n_outputs, learning_rate = get_parameters()
+    qnet = QNetwork(n_inputs, n_outputs, learning_rate)
+    qnet.to(torch.device(dev))
 
-try:
-    epsilon = epsilon_start
-    rewards, lengths, losses, epsilons = [], [], [], []
-    print('start training')
-    for i in range(num_episodes):
-        # init new episode
-        s, ep_reward, ep_loss = env.reset(), 0, 0
-        for j in range(episode_limit):
-            
-            # 1. do foward pass of current state to compute Q-values for all actions
-            qnet.optimizer.zero_grad()
-            Q = qnet(torch.from_numpy(s.reshape((1,1,210,160))).float())
-            
-            # 2. select action with epsilon-greedy strategy
-            a = Q.argmax().item() if np.random.rand() > epsilon else env.action_space.sample()
-            s1, r, done, _ = env.step(a)
-            
-            # 3. do forward pass for the next state
-            with torch.no_grad():
-                Q1 = qnet(torch.from_numpy(s.reshape((1,1,210,160))).float())
-            
-            # 4. set Q-target
-            q_target = Q.clone()
-            q_target[a] = r + gamma * Q1.max().item() * (not done)
-            
-            # 5. update network weights
-            loss = qnet.loss(Q, q_target)
-            loss.backward()
-            qnet.optimizer.step()
-            
-            # 6. bookkeeping
-            s = s1
-            ep_reward += r
-            ep_loss += loss.item()
-            if done: break
-        
-        # bookkeeping
-        #epsilon *= num_episodes/(i/(num_episodes/20)+num_episodes) # decrease epsilon
-        #epsilon -= 0.0001
-        epsilons.append(epsilon); rewards.append(ep_reward); lengths.append(j+1); losses.append(ep_loss)
-        if (i+1) % val_freq == 0: print('{:5d} mean training reward: {:5.2f}'.format(i+1, np.mean(rewards[-val_freq:])))
-    print('done')
-except KeyboardInterrupt:
-    print('interrupt')
+    try:
+        epsilon = epsilon_start
+        rewards, lengths, losses, epsilons = [], [], [], []
+        print('start training')
+        for i in range(num_episodes):
+            # init new episode
+            s, ep_reward, ep_loss = env.reset(), 0, 0
+            for j in range(episode_limit):
+
+                # 1. do foward pass of current state to compute Q-values for all actions
+                qnet.optimizer.zero_grad()
+                Q = qnet(torch.from_numpy(s.reshape((1,1,210,160))).float())
+
+                # 2. select action with epsilon-greedy strategy
+                a = Q.argmax().item() if np.random.rand() > epsilon else env.action_space.sample()
+                s1, r, done, _ = env.step(a)
+
+                # 3. do forward pass for the next state
+                with torch.no_grad():
+                    Q1 = qnet(torch.from_numpy(s.reshape((1,1,210,160))).float())
+
+                # 4. set Q-target
+                q_target = Q.clone()
+                q_target[a] = r + gamma * Q1.max().item() * (not done)
+
+                # 5. update network weights
+                loss = qnet.loss(Q, q_target)
+                loss.backward()
+                qnet.optimizer.step()
+
+                # 6. bookkeeping
+                s = s1
+                ep_reward += r
+                ep_loss += loss.item()
+                if done: break
+
+            # bookkeeping
+            #epsilon *= num_episodes/(i/(num_episodes/20)+num_episodes) # decrease epsilon
+            #epsilon -= 0.0001
+            epsilons.append(epsilon); rewards.append(ep_reward); lengths.append(j+1); losses.append(ep_loss)
+            if (i+1) % val_freq == 0: print('{:5d} mean training reward: {:5.2f}'.format(i+1, np.mean(rewards[-val_freq:])))
+        print('done')
+    except KeyboardInterrupt:
+        print('interrupt')
 
 
-torch.save(qnet.state_dict(), 'qnet.pt',_use_new_zipfile_serialization=False)
+    torch.save(qnet.state_dict(), 'qnet.pt',_use_new_zipfile_serialization=False)
