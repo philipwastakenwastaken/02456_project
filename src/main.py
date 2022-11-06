cc80320cc82573480f6bdb09c8f4cdeb19876754
Philip Tofte Nielsen <philiptofte@hotmail.com>
2022-11-06 12:57:28 +0100

    Change project folder structure: added src folder

---
+++ src/main.py
@@ -0,0 +1,20 @@
+
+
+
+class Session:
+
+    def __init__(self, config):
+
+        train_params = config.training
+        model_params = config.model
+        eval_params = config.evaluate
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
