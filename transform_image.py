ac0c75cf272d092648e9319f40f6f1db7f7fe3bb
Marcrulo <marcus@presutti.eu>
2022-11-14 22:17:06 +0100

    removed files

--- transform_image.py
+++
@@ -1,7 +0,0 @@
-from skimage.transform import rescale
-
-# crop and rescale image
-def transform(img):
-    img = rescale(img, 0.25, anti_aliasing=False)*255
-    img = img[5:36,2:-2] #asterix
-    return img
