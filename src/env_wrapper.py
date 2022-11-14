865c6311b82bea76b0e0b319261440e916153491
Philip Tofte Nielsen <philiptofte@hotmail.com>
2022-11-14 09:27:17 +0100

    Preliminary cleanup

--- src/env_wrapper.py
+++ src/env_wrapper.py
@@ -2,14 +2,30 @@ import gym
 from skimage.transform import resize
 import numpy as np
 import os
+import constant
+from abc import ABC, abstractmethod
 
+class BaseWrapper(gym.wrapper, ABC):
+
+    @abstractmethod
+    def step(self, action):
+        pass
+
+    @abstractmethod
+    def reset(self):
+        pass
+
+    def binarize(self, s):
+        s[s>constant.BINARY_THRESHOLD] = 1
+        s[s<=constant.BINARY_THRESHOLD] = 0
+        return s
 
 class CropWrapper(gym.Wrapper):
     def __init__(self, env):
         super().__init__(env)
         self.env = env
         self.action_space = gym.spaces.Discrete(5)
-        
+
     def step(self, action):
         s, r, done, info = self.env.step(action)
         s = s[6:170,5:-5]
@@ -20,7 +36,7 @@ class CropWrapper(gym.Wrapper):
         for i in range(65):
             self.env.step(0)
         s = s[6:170,5:-5]
-        return s 
+        return s
 
 
 class StretchWrapper(gym.Wrapper):
@@ -28,10 +44,10 @@ class StretchWrapper(gym.Wrapper):
         super().__init__(env)
         self.env = env
         self.action_space = gym.spaces.Discrete(5)
-        
+
     def step(self, action):
         s, r, done, info = self.env.step(action)
-        s = resize(s, (72,72), anti_aliasing=False)
+        s = resize(s, constant.RESIZE_SHAPE, anti_aliasing=False)
         s[s>0.3]=1
         s[s<=0.3]=0
         return s, r, done, info
@@ -40,10 +56,10 @@ class StretchWrapper(gym.Wrapper):
         s = self.env.reset()
         for i in range(65):
             self.env.step(0)
-        s = resize(s, (72,72), anti_aliasing=False)
+        s = resize(s, constant.RESIZE_SHAPE, anti_aliasing=False)
         s[s>0.3]=1
         s[s<=0.3]=0
-        return s 
+        return s
 
 
 class ResizeWrapper(gym.Wrapper):
@@ -51,11 +67,11 @@ class ResizeWrapper(gym.Wrapper):
         super().__init__(env)
         self.env = env
         self.action_space = gym.spaces.Discrete(5)
-        
+
     def step(self, action):
         s, r, done, info = self.env.step(action)
         s = s[6:170,5:-5]
-        s = resize(s, (72,72), anti_aliasing=False)
+        s = resize(s, constant.RESIZE_SHAPE, anti_aliasing=False)
         s[s>0.3]=1
         s[s<=0.3]=0
         return s, r, done, info
@@ -65,7 +81,7 @@ class ResizeWrapper(gym.Wrapper):
         for i in range(65):
             self.env.step(0)
         s = s[6:170,5:-5]
-        s = resize(s, (72,72), anti_aliasing=False)
+        s = resize(s, constant.RESIZE_SHAPE, anti_aliasing=False)
         s[s>0.3]=1
         s[s<=0.3]=0
-        return s 
+        return s
